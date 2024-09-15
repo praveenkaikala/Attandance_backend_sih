fbfefb1263dc2f454ba65bd208c13e7196610f0b
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2024-09-15 18:26:17 +0530

    update location model

--- models/locations.js
+++ models/locations.js
@@ -1,3 +1,4 @@
+// models/Location.js
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
@@ -5,12 +6,15 @@ const locationSchema = new Schema({
   name: { type: String, required: true },
   type: { type: String, enum: ['office', 'offsite'], required: true },
   location: {
-    type: { type: String, enum: ['Point'], required: true },
-    longitude: { type: Number, required: true }, 
-    latitude: { type: Number, required: true },
+    type: { type: String, enum: ['Point'], required: true }, // GeoJSON type
+    coordinates: { type: [Number], required: true }, // [longitude, latitude]
   },
+},
+{
+    timeStamp:true,
 });
 
+// Apply 2dsphere index on the coordinates field
 locationSchema.index({ location: '2dsphere' });
 
 module.exports = mongoose.model('Location', locationSchema);
