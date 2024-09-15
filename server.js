398e8f0e183ba0b8c19e449baffd30fe66aab2c1
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2024-09-15 18:27:41 +0530

    server

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express=require('express')
 const app=express()
 const cors=require('cors');
 const connectDB = require('./config/dbConfig');
-
-
+const fileUpload = require('express-fileupload');
+const employeeRoutes = require('./routes/employeeRoutes');
+const locationRoutes = require('./routes/locationRoutes');
 app.use(cors())
 app.use(express.json());
-
+app.use(fileUpload());
 app.listen(5000,()=>{
     console.log("server running")
 })
+app.use('/v1/api/employees', employeeRoutes);
+app.use('/api/locations', locationRoutes);
 connectDB();
