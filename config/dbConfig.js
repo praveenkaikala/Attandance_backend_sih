0c04a74043b0d9ab4d35505bd4bca3972c751f9c
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2024-09-17 10:18:27 +0530

    connection  issue

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.Mongo_uri, {
       dbName:"Attendance"
     });
 
--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -42,7 +42,46 @@ const createEmployee = async (req, res) => {
     res.status(500).json({ message: 'Server error' });
   }
 };
+const createManager = async (req, res) => {
+  try {
+    const { name, email, officeLocationId,phone,password } = req.body;
+    const existingEmployee = await Employee.findOne({ email });
+    if (existingEmployee) {
+      return res.status(400).json({ message: 'Employee with this email already exists.' });
+    }
+    let photoUrl = null;
+    // if (photo) {
+    //   const result = await cloudinary.uploader.upload(photo, {
+    //     folder: 'employees',
+    //     width: 500,
+    //     crop: 'scale',
+    //   });
+    //   photoUrl = result.secure_url; 
+    // }
 
+    const employee = new Employee({
+      name,
+      photo: photoUrl, 
+      email,
+      role:"manager",
+      officeLocationId,
+      phone,
+      managerId: null,
+      password
+    });
+
+    await employee.save();
+
+    res.status(201).json({
+      message: 'Employee created successfully.',
+      employee,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+}
 module.exports = {
   createEmployee,
+  createManager
 };
